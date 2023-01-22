90846ee2a15b95e86afdc7f1be77237e4006496e
DreLaSam <119361451+DreLaSam@users.noreply.github.com>
2023-01-22 08:27:17 -0500

    Create ServerPage.js

---
+++ src/Components/ServerPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function About () {
+    return (
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+}
+
+export default About
